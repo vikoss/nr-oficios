079c9166f95c7f16bffd4e4ad9cbcdec8009060b
vikoss <vikoss.yiss@gmail.com>
2022-02-16 21:43:29 -0600

    add fpdi example for test

---
+++ app/Http/Controllers/PDFController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use setasign\Fpdi\Fpdi;
+
+class PDFController extends Controller
+{
+    public function index(Request $request)
+    {   //return $request->file('sign')->path();
+        // initiate FPDI
+        $pdf = new FPDI();
+        // add a page
+        $pdf->AddPage();
+        // set the source file
+        $pdf->setSourceFile($request->file('pdf')->path());
+        // import page 1
+        $tplIdx = $pdf->importPage(1);
+        // use the imported page and place it at position 0,0 with a width of 210 mm (full page)
+        $pdf->useTemplate($tplIdx, 0, 0, 210);
+        // add signature
+        // $pdf->Image($request->file('sign')->path());
+        $pdf->Image($request->file('sign')->path(), 95, 185, 20, 0, 'PNG');
+        //$pdf->Image('http://chart.googleapis.com/chart?cht=p3&chd=t:60,40&chs=250x100&chl=Hello|World',60,30,90,0,'PNG');
+
+
+        return $pdf->Output();
+    }
+}
