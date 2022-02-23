ac71b23e9e24b94d68c8b650f38800d9f0d45bd0
vikoss <vikoss.yiss@gmail.com>
2022-02-23 13:52:41 -0600

    add route to test mail

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/test', [PDFController::class, 'index']);
+Route::get('/notified', [PDFController::class, 'notified']);
