b9625e167d6987777445d1a54125e5d95ea2fefe
vikoss <vikoss.yiss@gmail.com>
2022-03-01 16:02:29 -0600

    add email relationship method with notification

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Notification extends Model
 {
     use HasFactory;
+
+    public function emails()
+    {
+        return $this->hasMany(Email::class);
+    }
 }
