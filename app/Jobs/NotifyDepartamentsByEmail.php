5bee4b53f2fdc66553abc922b93979b4ebc81769
vikoss <vikoss.yiss@gmail.com>
2022-03-01 16:00:31 -0600

    add email notification in job

--- app/Jobs/NotifyDepartamentsByEmail.php
+++ app/Jobs/NotifyDepartamentsByEmail.php
@@ -2,25 +2,31 @@
 
 namespace App\Jobs;
 
+use App\Models\Notification;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Carbon\Carbon;
+use App\Mail\NotifiedDepartments;
+use Illuminate\Support\Facades\Mail;
 
 class NotifyDepartamentsByEmail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $notification;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Notification $notification)
     {
-        //
+        $this->notification = $notification;
     }
 
     /**
@@ -30,6 +36,10 @@ class NotifyDepartamentsByEmail implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $this->notification->emails()->whereNull('sent_at')->get()->each(function ($email, $key) {
+            Mail::to($email->to)->send(new NotifiedDepartments($this->notification));
+            $email->sent_at = Carbon::now();
+            $email->save();
+        });
     }
 }
