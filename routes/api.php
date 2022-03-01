5181707676e4a029c4a61337935d1a99f6dc7689
vikoss <vikoss.yiss@gmail.com>
2022-03-01 16:11:15 -0600

    add routes to emails and notifications

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PDFController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\NotificationEmailController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +23,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/test', [PDFController::class, 'index']);
 Route::get('/notified', [PDFController::class, 'notified']);
+
+Route::post('/sign', [PDFController::class, 'sign']);
+Route::post('/users/{user}/notifications', [PDFController::class, 'store']);
+Route::post('/notifications/{notification}/notify', [NotificationController::class, 'notify']);
+Route::post('/notifications/{notification}/emails', [NotificationEmailController::class, 'store']);
+Route::put('/emails/{email}', [EmailController::class, 'update']);
+Route::get('/emails/{email}', [EmailController::class, 'show']);
