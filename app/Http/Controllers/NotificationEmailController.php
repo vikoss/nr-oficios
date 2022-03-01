40a5f799907d7192ce6518c49b7abb66df7bd267
vikoss <vikoss.yiss@gmail.com>
2022-03-01 16:07:37 -0600

    add method for mass assignment of emails

--- app/Http/Controllers/NotificationEmailController.php
+++ app/Http/Controllers/NotificationEmailController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Notification;
 use Illuminate\Http\Request;
 
 class NotificationEmailController extends Controller
 {
-    //
+    public function store(Notification $notification, Request $request)
+    {
+        // Add validation of request
+        /* $data = $request->validate([
+            "name"    => "required|array|min:3",
+            "name.*"  => "required|string|distinct|min:3",
+        ]); */
+
+        return $notification->emails()->createMany($request->all());
+    }
 }
