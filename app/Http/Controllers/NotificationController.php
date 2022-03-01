6f00847ad3d5fa1af996fc812d65c0b9411cc68a
vikoss <vikoss.yiss@gmail.com>
2022-03-01 16:08:58 -0600

    add method to call notify departaments by email job

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\NotificationRequest;
 use App\Models\Notification;
+use App\Mail\NotifiedDepartments;
+use App\Models\Email;
+use Illuminate\Support\Facades\Mail;
+use Carbon\Carbon;
+use App\Jobs\NotifyDepartamentsByEmail;
 
 class NotificationController extends Controller
 {
@@ -12,5 +17,12 @@ class NotificationController extends Controller
     {
         // Primero subir al S3 el doc
         return Notification::create($notification->validated());
+    }
+
+    public function notify(Notification $notification, Request $request)
+    {
+        NotifyDepartamentsByEmail::dispatch($notification);
+
+        return response()->json(['message' => 'Notification enqueue successfully'], 202);
     }
 }
