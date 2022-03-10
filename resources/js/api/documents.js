2401d13cec7e741bf68f7c5adf08a23f4637616b
vikoss <vikoss.yiss@gmail.com>
2022-03-09 22:09:26 -0600

    add documents api

---
+++ resources/js/api/documents.js
@@ -0,0 +1,29 @@
+import { post, get } from 'axios';
+
+const signDocument = (document) => new Promise((resolve, reject) => {
+  const body = new FormData()
+  body.append('pdf', document)
+  post('http://localhost:9009/api/sign', body, {
+    headers: {
+      Authorization: 'Bearer ${JWT()}',
+    },
+  })
+    .then(({ data }) => {
+      resolve(data);
+    })
+    .catch(({ response }) => reject(response));
+});
+
+const me = () => new Promise((resolve, reject) => {
+  get('http://localhost:9009/api/auth/me', {
+    headers: {
+      Authorization: `Bearer ${JWT()}`,
+    },
+  })
+    .then(({ data }) => {
+      resolve(data);
+    })
+    .catch(({ response }) => reject(response));
+});
+
+export { signDocument, me };
