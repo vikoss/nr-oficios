7d7fca5130c26328952c737f74a0a79119c24775
vikoss <vikoss.yiss@gmail.com>
2022-03-11 12:09:50 -0600

    add api services to get notifications

--- resources/js/api/notifications.js
+++ resources/js/api/notifications.js
@@ -16,6 +16,42 @@ const storeNotification = (notification) => new Promise((resolve, reject) => {
     .catch(({ response }) => reject(response));
 });
 
+const getNotifications = () => new Promise((resolve, reject) => {
+  get(`http://localhost:9009/api/users/${userId}/notifications`, {
+    headers: {
+      Authorization: 'Bearer ${JWT()}',
+    },
+  })
+    .then(({ data }) => {
+      resolve(data);
+    })
+    .catch(({ response }) => reject(response));
+});
+
+const getNotification = (notificationId) => new Promise((resolve, reject) => {
+  get(`http://localhost:9009/api/notifications/${notificationId}`, {
+    headers: {
+      Authorization: 'Bearer ${JWT()}',
+    },
+  })
+    .then(({ data }) => {
+      resolve(data);
+    })
+    .catch(({ response }) => reject(response));
+});
+
+const getEmailsNotification = (notificationId) => new Promise((resolve, reject) => {
+  get(`http://localhost:9009/api/notifications/${notificationId}/emails`, {
+    headers: {
+      Authorization: 'Bearer ${JWT()}',
+    },
+  })
+    .then(({ data }) => {
+      resolve(data);
+    })
+    .catch(({ response }) => reject(response));
+});
+
 const notify = (notificationId) => new Promise((resolve, reject) => {
   post(`http://localhost:9009/api/notifications/${notificationId}/notify`, {
     headers: {
@@ -28,4 +64,4 @@ const notify = (notificationId) => new Promise((resolve, reject) => {
     .catch(({ response }) => reject(response));
 });
 
-export { storeNotification, notify };
+export { storeNotification, notify, getNotifications, getNotification, getEmailsNotification };
