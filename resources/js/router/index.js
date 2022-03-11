e40fe60e4f1819a7fdddd54f0b5416d8639e9525
vikoss <vikoss.yiss@gmail.com>
2022-03-11 12:11:27 -0600

    add routes for notifications and emails

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -7,29 +7,29 @@ const routes = [
     component: () => import ('./../views/Home.vue'),
   },
   {
+    path: '/notificaciones/:notification/correos/:email',
+    name: 'Emails',
+    component: () => import ('./../views/Emails/Details.vue'),
+  },
+  {
+    path: '/notificaciones/:notification',
+    name: 'NotificationDetails',
+    component: () => import ('./../views/Notifications/Details.vue'),
+  },
+  {
     path: '/notificaciones',
     name: 'Notifications',
-    component: () => import ('./../views/Notifications.vue'),
-  },
-  {
-    path: '/notificaciones/{notification}',
-    name: 'NotificationDetails',
-    component: () => import ('./../views/NotificationDetails.vue'),
-  },
-  {
-    path: '/notificaciones/{notification}/correos/{email}',
-    name: 'Email',
-    component: () => import ('./../views/Email.vue'),
+    component: () => import ('./../views/Notifications/Index.vue'),
   },
   {
     path: '/notificar',
     name: 'Notify',
-    component: () => import ('./../views/Notify.vue'),
+    component: () => import ('../views/Notifications/Notify.vue'),
   },
   {
-    path: '/validacion/{email}',
-    name: 'Notifications',
-    component: () => import ('./../views/Notifications.vue'),
+    path: '/validacion/:email',
+    name: 'EmailValidation',
+    component: () => import ('./../views/Emails/Validation.vue'),
   },
 ];
 
