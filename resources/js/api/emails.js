afa4f59f9864554ae75dd81edcabd7eae7fd66d1
vikoss <vikoss.yiss@gmail.com>
2022-03-11 12:07:16 -0600

    add api services to update and get email

--- resources/js/api/emails.js
+++ resources/js/api/emails.js
@@ -1,4 +1,4 @@
-import { post, get } from 'axios';
+import { post, get, put } from 'axios';
 
 const storeEmails = ({ notificationId, emails }) => new Promise((resolve, reject) => {
   post(`http://localhost:9009/api/notifications/${notificationId}/emails`, emails, {
@@ -12,10 +12,10 @@ const storeEmails = ({ notificationId, emails }) => new Promise((resolve, reject
     .catch(({ response }) => reject(response));
 });
 
-const me = () => new Promise((resolve, reject) => {
-  get('http://localhost:9009/api/auth/me', {
+const getEmail = (emailId) => new Promise((resolve, reject) => {
+  get(`http://localhost:9009/api/emails/${emailId}`, {
     headers: {
-      Authorization: `Bearer ${JWT()}`,
+      Authorization: 'Bearer ${JWT()}',
     },
   })
     .then(({ data }) => {
@@ -24,4 +24,16 @@ const me = () => new Promise((resolve, reject) => {
     .catch(({ response }) => reject(response));
 });
 
-export { storeEmails, me };
+const updateEmail = (email) => new Promise((resolve, reject) => {
+  put(`http://localhost:9009/api/emails/${email.id}`, email, {
+    headers: {
+      Authorization: 'Bearer ${JWT()}',
+    },
+  })
+    .then(({ data }) => {
+      resolve(data);
+    })
+    .catch(({ response }) => reject(response));
+});
+
+export { storeEmails, getEmail, updateEmail };
