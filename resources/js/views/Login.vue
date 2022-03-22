456b1e37639c1c99a7c3de34acc1efd603ffca71
vikoss <vikoss.yiss@gmail.com>
2022-03-21 21:27:23 -0600

    add login view with service authenticate

--- resources/js/views/Login.vue
+++ resources/js/views/Login.vue
@@ -1,9 +1,33 @@
 <template>
-  
+  <div>
+    <input v-model="credentials.email" type="text" name="email" id="email">
+    <input v-model="credentials.password" type="password" name="password" id="password">
+    <input type="button" value="Entrar" @click="login">
+  </div>
 </template>
 
 <script>
+import { reactive } from 'vue'
+import { authenticate } from './../api/authenticate'
+import { useRouter } from 'vue-router'
+
 export default {
+  name: 'Login',
+  setup() {
+    const router = useRouter()
+    const credentials = reactive({
+      email: '',
+      password: '',
+    })
+
+    const login = async () => {
+      await authenticate(credentials)
+      router.push({ name: 'Home' })
+
+    }
+
+    return { credentials, login };
+  },
 
 }
 </script>
