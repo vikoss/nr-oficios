681c10b98b7830085048434fd76a7628998e2d58
vikoss <vikoss.yiss@gmail.com>
2022-03-21 21:04:37 -0600

    add relationsships to position model

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Position extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the phone associated with the user.
+     */
+    public function departament()
+    {
+        //return $this->hasOne(Departament::class);
+        return $this->belongsTo(Departament::class);
+    }
 }
