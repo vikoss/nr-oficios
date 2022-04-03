0f9bdf78767ef4651a7d32f980f611ee4f5330ed
vikoss <vikoss.yiss@gmail.com>
2022-04-03 18:47:01 -0500

    add header base web component

---
+++ resources/js/components/HeaderBase.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="bg-wine h-20">
+    <ul class="flex justify-between">
+      <li class="ml- ml-11">
+        HOME
+      </li>
+      <li class="mr-11">
+        USER@USER.COM
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
