097b08a8c0572e843218277e3a2a3e97e04c4035
vikoss <vikoss.yiss@gmail.com>
2022-04-03 15:37:51 -0500

    add main colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
 module.exports = {
-  content: [],
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
   theme: {
+    colors: {
+      'wine': '#54191b',
+      'wine-100': '#691c32',
+      'wine-200': '#8e1537',
+      'wine-logo': '#73051c',
+      'gray': '#a0a0a0',
+      'gray-100': '#d5d6d1',
+      'gray-200': '#e0e2e0',
+      'white': '#ffffff'
+    },
     extend: {},
   },
   plugins: [],
