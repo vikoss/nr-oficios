73956b7b317051e55591d9b32c26540d16f4367d
vikoss <vikoss.yiss@gmail.com>
2022-04-03 22:12:01 -0500

    add input file web component

---
+++ resources/js/components/InputFile.vue
@@ -0,0 +1,43 @@
+<template>
+  <label
+    :for="id"
+    class="bg-wine rounded-md text-white text-base
+    font-bold flex justify-center items-center h-10 cursor-pointer"
+  >
+    <input
+      class="hidden"
+      type="file"
+      :accept="accept"
+      @change="onChange"
+      :name="id"
+      :id="id"
+    >
+    <cloud-svg class="mr-3.5 h-6 w-6" />
+    Agregar archivo
+  </label>
+</template>
+
+<script>
+import CloudSvg from './../svg/Cloud.vue'
+
+export default {
+  props: {
+    id: {
+      type: String,
+      default: 'file_passport',
+    },
+    accept: {
+      type: String,
+      default: '.pdf'
+    },
+  },
+  components: { CloudSvg },
+  setup(props, { emit }) {
+    const onChange = ({ target }) => {
+      emit('file', { value: target.files[0], name: props.id })
+    };
+
+    return { onChange }
+  },
+};
+</script>
