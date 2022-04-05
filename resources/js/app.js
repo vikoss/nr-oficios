2c3a1afa4db1a4c0ca4413e49a79ecf18e51800d
vikoss <vikoss.yiss@gmail.com>
2022-04-05 18:09:04 -0500

    add middleware for routes

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
+import { JWT } from './helpers/localStorage'
+
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'Login' && !JWT()) next({ name: 'Login' });
+  else next();
+});
 
 createApp(App)
   .use(router)
