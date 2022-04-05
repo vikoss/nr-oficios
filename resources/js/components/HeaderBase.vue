18b4d68461c87a93e84b7421791f65a1a534d400
vikoss <vikoss.yiss@gmail.com>
2022-04-05 18:50:56 -0500

    add view for header base

--- resources/js/components/HeaderBase.vue
+++ resources/js/components/HeaderBase.vue
@@ -1,22 +1,34 @@
 <template>
-  <header class="bg-wine h-20">
+  <header class="bg-wine h-20 flex flex-col justify-center">
     <ul class="flex justify-between">
       <li class="ml- ml-11">
-        HOME
+        <router-link :to="{ name: 'Home' }" class="text-white text-xl">
+          Inicio
+        </router-link>
       </li>
       <li class="mr-11">
-        USER@USER.COM
+        <div class="flex items-center">
+          <p class="mr-3 text-white text-sm sm:text-lg font-light">
+            {{ user.email }}
+          </p>
+          <arrow-bottom-svg class="mt-1 cursor-pointer" />
+        </div>
       </li>
     </ul>
   </header>
 </template>
 
 <script>
-export default {
+import { inject } from 'vue'
+import ArrowBottomSvg from './../svg/ArrowBottom.vue'
 
+export default {
+  name: 'HeaderBase',
+  components: { ArrowBottomSvg },
+  setup() {
+    const user = inject('currentUser', {})
+
+    return { user }
+  },
 }
 </script>
-
-<style>
-
-</style>
