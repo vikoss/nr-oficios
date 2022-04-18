057b7e37421a77993171a82d04447baaadf70b4b
vikoss <vikoss.yiss@gmail.com>
2022-04-17 19:01:20 -0500

    add color error

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       'wine-100': '#691c32',
       'wine-200': '#8e1537',
       'wine-logo': '#73051c',
+      'red-error': '#d93025',
       'gray': '#a0a0a0',
       'gray-100': '#d5d6d1',
       'gray-200': '#e0e2e0',
