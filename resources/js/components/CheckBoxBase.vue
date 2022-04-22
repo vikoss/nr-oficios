fbcd5a94b48d12aa85425a65dfcae99cfbde5748
vikoss <vikoss.yiss@gmail.com>
2022-04-22 17:44:58 -0500

    add prop small for checkbox web component

--- resources/js/components/CheckBoxBase.vue
+++ resources/js/components/CheckBoxBase.vue
@@ -2,13 +2,14 @@
   <input
     :id="id"
     :checked="modelValue"
-    class="h-6 w-6 bg-no-repeat bg-center bg-contain appearance-none outline-0 align-middle mr-3"
-    :class="{ 'bg-checkbox-not-checked': !modelValue, 'bg-checkbox-checked': !!modelValue }"
+    class="h-6 w-6 bg-no-repeat bg-center bg-contain appearance-none outline-0 align-middle cursor-pointer"
+    :class="{ 'bg-checkbox-not-checked': !modelValue, 'bg-checkbox-checked': !!modelValue, 'mr-3': !small }"
     type="checkbox"
     @input="(event) => $emit('update:modelValue', event.target.checked)"
   >
   <label
-    class="text-base text-blue font-normal"
+    class="font-normal"
+    :class="{ 'text-xs': small, 'text-base': !small, 'text-gray': !modelValue, 'text-black': modelValue }"
     :for="id"
   >
     {{ label }}
@@ -28,6 +29,10 @@ export default {
       default: '',
     },
     modelValue: {
+      type: Boolean,
+      default: false,
+    },
+    small: {
       type: Boolean,
       default: false,
     },
