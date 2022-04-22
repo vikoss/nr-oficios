8aa9429c6f38d685f42a13538c37caa5f5e3e4bf
vikoss <vikoss.yiss@gmail.com>
2022-04-21 23:28:41 -0500

    modify header base web component

--- resources/js/components/HeaderBase.vue
+++ resources/js/components/HeaderBase.vue
@@ -1,20 +1,22 @@
 <template>
   <header class="bg-wine h-20 flex flex-col justify-center">
     <ul class="flex justify-between">
-      <li class="ml- ml-11">
-        <router-link :to="{ name: 'Home' }" class="text-white text-xl">
-          Inicio
+      <li class="ml-11">
+        <router-link :to="{ name: 'Home' }">
+          <img src="./../../../public/images/logo-nr.png" alt="logo" class="h-16">
         </router-link>
       </li>
       <li class="mr-11">
-        <div class="flex items-center relative">
-          <p class="mr-3 text-white text-sm sm:text-base font-light">
-            {{ user.email }}
-          </p>
-          <arrow-bottom-svg class="mt-1 cursor-pointer w-4 h-4" @click="app.showWindowLogOut = !app.showWindowLogOut" />
-        </div>
-        <div v-show="app.showWindowLogOut" class="text-right absolute right-10">
-          <input class="bg-white px-3 py-1 text-base hover:bg-gray-300 cursor-pointer rounded-sm shadow-lg shadow-wine" type="button" value="Salir" @click="logOut">
+        <div class="flex justify-center h-full relative">
+          <div class="flex items-center">
+            <p class="mr-3 text-white text-sm sm:text-base font-light">
+              {{ user.email }}
+            </p>
+            <arrow-bottom-svg class="mt-1 cursor-pointer w-4 h-4" @click="app.showWindowLogOut = !app.showWindowLogOut" />
+          </div>
+          <div v-show="app.showWindowLogOut" class="text-right absolute top-12 right-0">
+            <input class="bg-white px-4 py-1 text-base hover:bg-gray-300 cursor-pointer rounded-sm shadow-lg shadow-wine" type="button" value="Salir" @click="logOut">
+          </div>
         </div>
       </li>
     </ul>
