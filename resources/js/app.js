80219fe367e5b1cc7feb970c182d66afef8956a4
Karly-Dan <daniela.kar99@gmail.com>
2023-02-08 16:33:17 -0600

    modification in app.js to display the new route

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,42 +7,42 @@ import { JWT } from './helpers/localStorage'
 // Creates a `nextMiddleware()` function which not only
 // runs the default `next()` callback but also triggers
 // the subsequent Middleware function.
-function nextFactory(context, middleware, index) {
-  const subsequentMiddleware = middleware[index]
-  // If no subsequent Middleware exists,
-  // the default `next()` callback is returned.
-  if (!subsequentMiddleware) return context.next
+// function nextFactory(context, middleware, index) {
+//   const subsequentMiddleware = middleware[index]
+//   // If no subsequent Middleware exists,
+//   // the default `next()` callback is returned.
+//   if (!subsequentMiddleware) return context.next
 
-  return (...parameters) => {
-    // Run the default Vue Router `next()` callback first.
-    context.next(...parameters)
-    // Then run the subsequent Middleware with a new
-    // `nextMiddleware()` callback.
-    const nextMiddleware = nextFactory(context, middleware, index + 1)
-    subsequentMiddleware({ ...context, next: nextMiddleware })
-  }
-}
+//   return (...parameters) => {
+//     // Run the default Vue Router `next()` callback first.
+//     context.next(...parameters)
+//     // Then run the subsequent Middleware with a new
+//     // `nextMiddleware()` callback.
+//     const nextMiddleware = nextFactory(context, middleware, index + 1)
+//     subsequentMiddleware({ ...context, next: nextMiddleware })
+//   }
+// }
 
-router.beforeEach((to, from, next) => {
-  if (to.name === 'EmailValidation') next()
-  else if (to.name !== 'Login' && !JWT()) next({ name: 'Login' })
-  else if (to.meta.middleware) {
-    const middleware = Array.isArray(to.meta.middleware)
-      ? to.meta.middleware
-      : [to.meta.middleware]
+// router.beforeEach((to, from, next) => {
+//   if (to.name === 'EmailValidation') next()
+//   else if (to.name !== 'Login' && !JWT()) next({ name: 'Login' })
+//   else if (to.meta.middleware) {
+//     const middleware = Array.isArray(to.meta.middleware)
+//       ? to.meta.middleware
+//       : [to.meta.middleware]
 
-    const context = {
-      from,
-      next,
-      router,
-      to,
-    }
-    const nextMiddleware = nextFactory(context, middleware, 1)
+//     const context = {
+//       from,
+//       next,
+//       router,
+//       to,
+//     }
+//     const nextMiddleware = nextFactory(context, middleware, 1)
 
-    return middleware[0]({ ...context, next: nextMiddleware })
-  }
-  else next()
-})
+//     return middleware[0]({ ...context, next: nextMiddleware })
+//   }
+//   else next()
+// })
 
 createApp(App)
   .use(router)
